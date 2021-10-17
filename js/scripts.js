8354d2fdc6726503ac69661c70c1b67e315a7d09
Loren Zeman <Loren@lorencamille.net>
2021-10-17 14:30:57 -0400

    added: call modal with jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,13 @@ $(function () {
         }
     });
 });
+
+// Note to Prof: this was harder to figure out than anything else we've done so far, mainly because I don't think the instructions were very clear. And the lessons didn't really cover how Jquery worked or how the code could be changed from the lesson examples to do this. I honestly just googled until I found a similar answer, and then plugged & chugged until it worked. I still have zero clue why this answer worked and my previous attempts didn't. (I found my answer here: https://www.studentstutorial.com/jquery/jquery-show_hide_bootstarp_modal.php)
+
+$("#reserveButton").on("click", function () {
+    $('#reserveModal').modal('show');
+});
+
+$("#loginButton").on("click", function () {
+    $('#loginModal').modal('show');
+});
